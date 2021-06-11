c2676fa095a01d7f0611d1104c20aab0ddc6226a
actondev <chris.actondev@gmail.com>
2021-06-11 19:57:31 +0200

    common-postlude: surrounded main with exception handler

--- src/common-postlude.scm
+++ src/common-postlude.scm
@@ -1,1 +1,7 @@
-(main)
+(with-exception-handler (lambda args
+			  (display "exception: ")
+			  (display args)
+			  (newline)
+			  (flush-output-port (current-output-port))
+			  (exit 1))
+			main)
